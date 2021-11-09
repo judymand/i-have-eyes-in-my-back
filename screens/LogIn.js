fb47533fbd37e4ca8fcc162d48b776d20430f6a8
יהודית מנדלבוים <judy@yhwdyts-MacBook-Pro.local>
2021-11-09 22:55:05 +0200

    Added View container.

--- screens/LogIn.js
+++ screens/LogIn.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View, Button, TouchableWithoutFeedback, Keyboard  } from 'react-native';
 import style from '../styles/GlobalStyle'
 import Input from '../components/Input'
+import { Card } from '../components/Card'
  
 
 export const LogIn = () => {
@@ -12,15 +13,19 @@ export const LogIn = () => {
 
   return (
       <TouchableWithoutFeedback onPress={ () => { Keyboard.dismiss();}}>
-          <View style={style.container}>
-              <Text style={style.header} > Log In</Text>
-              <Text style={style.text} > Email:</Text>
-              <Input style={{color:'black' }}/>
-              <Text style={style.text} > Password:</Text>
-              <Input style={{color:'black' }}/>
-
-              <Button title="Log In"/>
-              
+         <View style={style.viewContainerCard}>
+          <Card>
+              <View style={{textAlign: "center", alignItems: 'center'}}>
+                  <Text style={style.header} > Log In</Text>
+                  <Text style={style.text} > Email:</Text>
+                  <Input style={{color:'black' }}/>
+                  <Text style={style.text} > Password:</Text>
+                  <Input style={{color:'black' }}/>
+                  <View style={style.button}>
+                    <Button title="Log In"/>
+                  </View>
+              </View>
+            </Card>
           </View>
         </ TouchableWithoutFeedback>
   );
--- screens/SignUp.js
+++ screens/SignUp.js
@@ -8,69 +8,102 @@ import Card from '../components/Card'
 
 export const SignUp = () => {
  
-  const [Email, SetEmail] = useState('')
-  const [Password, SetPassword] = useState('')
-  const [VerifyPassword, SetVerifyPassword] = useState('')
-  const [ValideEmail, SetValideEmail] = useState(true)
+  firstName
+  const [firstName, SetfirstName] = useState('')
+  const [lastName, SetLastName] = useState('')
+  const [email, SetEmail] = useState('')
+  const [password, SetPassword] = useState('')
+  const [verifyPassword, SetVerifyPassword] = useState('')
+  const [ValideEmail, SetValideEmail] = useState(false)
+  const [checkStrongPassword,setCheckStrongPassword] = useState('')
+  const [checkSamePassword,setCheckSamePassword] = useState(false)
 
 
   const checkPassword = (Password) => {
+    let strongPassword = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})')
+    let mediumPassword = new RegExp('((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{6,}))|((?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,}))')
     SetPassword(Password)
+      if(strongPassword.test(password)  === true) {
+        setCheckStrongPassword("green")
+      } else if(mediumPassword.test(password)  === true){
+        setCheckStrongPassword('blue')
+      } else{
+        setCheckStrongPassword('red')
+      }
   }
 
   const validateEmail  = (text) => {
 
     let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
     if (reg.test(text) === false) {
-      // SetValideEmail('red')
       SetValideEmail(false)
       SetEmail(text)
     }
     else {
       SetValideEmail(true)
       SetEmail(text)
-      // SetValideEmail('green') 
     }
   }
 
-  const verifyPassword = (Password) => {
-    SetVerifyPassword(Password)
-    // if(Password != VerifyPassword){
-    //   borderVerifyPassword 
-    // }
-
+  const samePassword = (text) => {
+    SetVerifyPassword(text) 
+    // console.log(password)
+    // console.log(verifyPassword)
+    // console.log(checkStrongPassword)
+    if(password === verifyPassword && checkStrongPassword === "green"){
+      setCheckSamePassword(true)
+    }
+    else{
+      setCheckSamePassword(false)
+    }
   }
 
   return (
     <TouchableWithoutFeedback onPress={ () => { Keyboard.dismiss();}}>
-      <Card> 
-        <View style={style.container}>
-              <Title style={style.header}>Sign Up</Title>
-              <Title  style={style.text}> Email: </Title>
-              <Input 
-              style={ Email === '' ? style.input : ValideEmail ? style.Valid : style.noValid} 
-              onChangeText={validateEmail}
-              value={Email}
-              />
-              <Title style={style.text} > Password:</Title>
-              <Input 
-              style={{color:'black' }} 
-              onChangeText={checkPassword}
-              value={Password}
-              />
-              <Title style={style.text} > Verify Password:</Title>
-              <Input 
-              style={{color:'black' }}
-              onChangeText={verifyPassword}
-              value={VerifyPassword}
-              /> 
-              <View style={style.button}>
-              <Button 
-                title="Sign Up"/>
-              </View>
-            
-          </View>
-        </Card>
+      <View style={style.viewContainerCard}>
+        <Card > 
+          <View style={{textAlign: "center", alignItems: 'center'}}>
+                <Title  style={{fontWeight: 'bold',fontSize: 22}}> Sign Up  </Title>
+                <Title  style={style.text}> First name: </Title>
+                <Input 
+                style={style.input} 
+                onChangeText={(text) => {SetfirstName(text)}}
+                value={firstName}
+                />
+
+                <Title  style={style.text}> Last name: </Title>
+                  <Input 
+                  style={style.input} 
+                  onChangeText={(text) => {SetLastName(text)}}
+                  value={lastName}
+                  />
+
+                <Title  style={style.text}> Email: </Title>
+                <Input 
+                style={ email === '' ? style.input : ValideEmail ? style.Valid : style.noValid} 
+                onChangeText={validateEmail}
+                value={email}
+                />
+                <Title style={style.text} > Password:</Title>
+                <Input 
+                style={ password === '' ? style.input : checkStrongPassword === 'red' ? style.noValid : checkStrongPassword === 'blue' ? style.mediumPasswordStyle : style.Valid } 
+                onChangeText={checkPassword}
+                value={password}
+                />
+                <Title style={style.text} > Verify Password:</Title>
+                <Input 
+                style={ verifyPassword === '' ? style.input : checkSamePassword ? style.Valid : style.noValid} 
+                onChangeText={samePassword}
+                value={verifyPassword}
+                /> 
+                <View style={style.button}>
+                <Button 
+                  title="Sign Up"/>
+                </View>
+              
+            </View>
+          </Card>
+        </View>
     </TouchableWithoutFeedback>
        
   );
