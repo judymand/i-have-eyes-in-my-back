f3893787fcb9f1c98549abd406c03350fd6f66ae
יהודית מנדלבוים <judy@yhwdyts-MacBook-Pro.local>
2021-11-09 22:47:09 +0200

    Change from View to ScrolView.

--- components/Card.js
+++ components/Card.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Platform  } from 'react-native';
+import { ScrollView  ,Platform  } from 'react-native';
 import style  from '../styles/GlobalStyle'
 
 const device =  Platform.OS === 'ios'
@@ -7,9 +7,9 @@ const device =  Platform.OS === 'ios'
 export const Card = (props) => {
 
   return (
-       <View style={[style.card, device ? style.cardIOS : style.cardAndroid]}>
+       <ScrollView style={[style.card, device ? style.cardIOS : style.cardAndroid]}>
          {props.children}
-       </View>
+       </ScrollView>
        
   );
 }
