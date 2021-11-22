b4faad448f267729739f1ccf2c54fc7e8f2f827f
judymand <judymand70@gmail.com>
2021-11-22 12:36:53 +0200

    Added title to home page.

--- Navigation/HomePageNavigator.js
+++ Navigation/HomePageNavigator.js
@@ -3,14 +3,19 @@ import { createAppContainer } from "react-navigation";
 import { HomePage } from '../screens/HomePage';
 import { SignUp } from '../screens/SignUp';
 import { LogIn } from '../screens/LogIn';
+import List from '../components/List';
 
 
 const HomePageNavigator = createStackNavigator({
     HomePage: {
         screen: HomePage,
+        navigationOptions: {
+            headerTitle: 'I have eyes in my back'
+        }
     },
     SignUp: SignUp,
     LogIn: LogIn ,
+    List: List,
 });
 
 export default createAppContainer(HomePageNavigator)
