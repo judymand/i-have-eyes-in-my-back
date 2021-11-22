804db59b4f238a6e9702a4b603e70da6f30b411d
judymand <judymand70@gmail.com>
2021-11-22 12:39:06 +0200

    Added bodyText, useState and transfer data.

--- screens/LogIn.js
+++ screens/LogIn.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import { Text, View, Button, TouchableWithoutFeedback, Keyboard  } from 'react-native';
+import React, { useState } from 'react';
+import { View, Button, TouchableWithoutFeedback, Keyboard  } from 'react-native';
 import style from '../styles/GlobalStyle'
-import Input from '../components/Input'
+import { Input } from '../components/Input'
 import { Card } from '../components/Card'
- 
+import { BodyText } from '../components/BodyText'
 
-export const LogIn = () => {
+const DATA = [
+  {
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "First Item",
+  },
+  {
+    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+    title: "Second Item",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Third Item",
+  },
+];
+
+export const LogIn = (props) => {
  
-    // const [Email, SetEmail] = useState('')
-    // const [Password, SetPassword] = useState('')
+  const [email, SetEmail] = useState('')
+  const [password, SetPassword] = useState('')
 
 
   return (
@@ -16,13 +31,20 @@ export const LogIn = () => {
          <View style={style.viewContainerCard}>
           <Card>
               <View style={{textAlign: "center", alignItems: 'center'}}>
-                  <Text style={style.header} > Log In</Text>
-                  <Text style={style.text} > Email:</Text>
-                  <Input style={{color:'black' }}/>
-                  <Text style={style.text} > Password:</Text>
-                  <Input style={{color:'black' }}/>
+                  <BodyText style={style.header} > Log In</BodyText>
+                  <BodyText style={style.Bodytext} > Email:</BodyText>
+                  <Input
+                  onChangeText={(text) => {SetEmail(text)}}
+                  value={email}
+                  />
+                  <BodyText style={style.Bodytext} > Password:</BodyText>
+                  <Input 
+                  onChangeText={(text) => {SetPassword(text)}}
+                  value={password}
+                  />
+
                   <View style={style.button}>
-                    <Button title="Log In"/>
+                    <Button title="Log In" onPress={ () => props.navigation.navigate({ routeName: 'List', params: { DATA: DATA } })}/>
                   </View>
               </View>
             </Card>
