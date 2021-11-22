a28f83cd9853bf7a7feadeea05540c2049f8ed76
judymand <judymand70@gmail.com>
2021-11-22 12:35:53 +0200

    Added class list screen.

---
+++ screens/ClassList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from '../styles/GlobalStyle'
+import { List } from '../components/List'
+
+export const ClassList = (props) => {
+
+  return (
+       <List>
+           
+       </List>
+       
+  );
+}
+  export default ClassList
