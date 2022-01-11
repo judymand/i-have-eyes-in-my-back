7d0d766372d846c79eb4142d5c53cf8c6c165052
judymand <judymand70@gmail.com>
2022-01-11 11:54:46 +0200

    Added Settings page.

---
+++ screens/Settings.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import style from '../styles/GlobalStyle'
+import { View } from 'react-native';
+
+
+export const Settings = (props) => {
+
+
+  return (
+      <View>
+
+      </View>
+       
+  );
+}
+  export default Settings
