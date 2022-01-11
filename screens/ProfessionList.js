2ae93ac7d404ba4ca97aa265dda34a9a56efa0c9
judymand <judymand70@gmail.com>
2022-01-11 11:54:07 +0200

    Added Profession List page.

---
+++ screens/ProfessionList.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import style from '../styles/GlobalStyle'
+import { List } from '../components/List'
+import { View } from 'react-native';
+
+
+export const ProfessionList = (props) => {
+
+  const [data, Setdata] = useState([])
+  const className = null
+
+  useEffect(() => {
+    fetch('http://10.0.0.5:3000/getClasses',
+    {
+      method:"POST",
+      headers:{
+        'Content-Type':'application/json'
+      }
+    })
+    .then(res => res.json())
+    .then(result => 
+      Setdata(result.classRoom)
+      )
+
+  }, [])
+
+
+  return (
+    
+    <List 
+    Data={data} 
+    type='className' 
+    num={2} 
+    onPress={props}/>
+       
+  );
+}
+  export default ProfessionList
