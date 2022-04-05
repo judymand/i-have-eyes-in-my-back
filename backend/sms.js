a1553766fc702088f7a94450bec02874525ea47c
shaked <shakedhamo@gmail.com>
2022-04-05 10:49:54 +0300

    message on time

--- backend/sms.js
+++ backend/sms.js
@@ -4,9 +4,22 @@
 // ZSRX_9PTwu@=Zc^12345678
 require('dotenv').config()
 const twilio=require('twilio');
+let now = new Date(); 
+console.log(now)
+const schedule = require('node-schedule');
+
+	const sdate=new Date(now)
+	sdate.setHours(17,0,0)//time to send message
+	console.log(sdate)
+	schedule.scheduleJob(sdate, ()=>{
+		console.log('start');
+		mesP()
+	console.log('end');
+	});
+
+ 
 var accountSid = process.env.TWILIO_ACCOUNT_SID; // Your Account SID from www.twilio.com/console
 var authToken = process.env.TWILIO_AUTH_TOKEN;   // Your Auth Token from www.twilio.com/console
-
 
 let Mymessage='judy modi'
 let Yournumber='+972523190543'
@@ -14,18 +27,19 @@ let MyNumber='+12184141637'
 
 
 
-console.log(".........")
-var client=new twilio(accountSid,authToken)
+function mesP(){
+	console.log(".........")
+	var client=new twilio(accountSid,authToken)
 
-client.messages
-.create({
-	from:MyNumber,
-	to:Yournumber,
-	body:Mymessage
+	client.messages
+	.create({
+		from:MyNumber,
+		to:Yournumber,
+		body:Mymessage
 })
-.then(message=>console.log(message.sid)).done();
-console.log(".........")
-
+	.then(message=>console.log(message.sid)).done();
+	console.log(".........")
+}
 
 
 
