7977e9b01d7c0aef29d41a10ea70e8ac3c5cfe59
shaked <shakedhamo@gmail.com>
2022-04-19 16:42:35 +0300

    sms done

--- backend/sms.js
+++ backend/sms.js
@@ -1,12 +1,13 @@
 // judi '+972543315931----+12184141637'
 //0523190543
+//0523194226
 //mmyt1785@gmail.com
 // ZSRX_9PTwu@=Zc^12345678
 
 const LessonSchema = require('./models/Lesson');
 const DailyAttendanceSchema = require('./models/DailyAttendance');
 const studentsListSchema=require("./models/studentList");
-
+const schedule = require('node-schedule');
 
 //add check Classname//
 
@@ -75,45 +76,21 @@ require('dotenv').config()
 	let sum_of_students_who_did_not_attend_class_but_is_in_school=0
 	let sum_of_students_who_did_not_attend_class_and_not_in_school=0
 
-// //  //הוספה של משתנה חדש ללסון
-// // new LessonSchema({
-// // 	className:"א2",
-// // 	profession:"חשבון",
-// // 	day:today,
-// // 	time:Ttime,
-// // 	students:[
-// // 		{
-// // 		name:"סיטארה אלייב",
-// // 		arrived:true
-// // 		},
-// // 		{
-// // 		name:"דודי ביטון",
-// // 		arrived:true
-// // 		},
-// // 		{
-// // 		name:"יהודית מנדלבוים",
-// // 		arrived:false
-// // 		}
-// // 	]
 
-// // }).save()
+    //let now = new Date(); 
+	//const sdate=new Date(now)
+	//sdate.setHours(17,0,0)//time to send message
 
-// let h="י-י"
-// let d="י י"
-// let a="ר"
-
-// a=h.replace(/\-/, ' ')
-// console.log(a);
-
-// //console.log (d.localeCompare(h,'he'))
-
-
-
+	schedule.scheduleJob('00 00 17 * * 0-5', ()=>{
+	console.log('start');
+		g()
+	console.log('end');
+	});
 
 
 // רשימה שהמצלמה עדכנה dailyAttendance
 
-g()
+
 async function g(){
 
 	let Rresults=await DailyAttendanceSchema.find({'day':today})//מצלמה שמזהה את מי הגיע
@@ -155,8 +132,6 @@ async function g(){
 	}
 
 
-
-
 	for(let i=0;i<Rresults[0].students.length;i++)
 	{
 		if(Rresults[0].students[i].arrive==true)
@@ -196,7 +171,7 @@ async function g(){
 			nameL.replace(/\-/, ' ')
 			if(nameP.localeCompare(nameL)==0)
 			{
-				List_of_students_who_did_not_attend_class_and_not_in_school[i].setphone(PhoneRresults.students[j].phone)
+				List_of_students_who_did_not_attend_class_and_not_in_school[i].setphone(PhoneRresults.students[j].phone.replace(/\-/, ''))
 				continue;
 			}
 		}
@@ -221,148 +196,148 @@ async function g(){
 		}
 	}
 
-
-
 	console.log("IN G()")
 	console.log(List_of_students_who_did_not_attend_class_but_is_in_school)
-	//SMS_SENDER_Breeze_from_class()
-	//SMS_SENDER_Breeze_from_School()
-	//console.log(".......",inSchool,"####............####",not_in_class,".......")
-	//console.log(".......",inSchool,".......",List_of_students_who_did_not_attend_class_and_not_in_school,"............",not_in_class,".......")
+	SMS_SENDER_Breeze_from_class()
+	SMS_SENDER_Breeze_from_School()
 	console.log(List_of_students_who_did_not_attend_class_and_not_in_school)
 }
 
 
+function SMS_SENDER_Breeze_from_class(){
+console.log("in SMS_SENDER_Breeze_from_class()",sum_of_students_who_did_not_attend_class_but_is_in_school)
+for(let i=0;i<sum_of_students_who_did_not_attend_class_but_is_in_school;i++)
+{
+let phone=List_of_students_who_did_not_attend_class_but_is_in_school[i].phone.replace(/0/, "+972")
+//let name=List_of_students_who_did_not_attend_class_but_is_in_school[i].name
+//let profession=List_of_students_who_did_not_attend_class_but_is_in_school[i].profession
+//let size_of_profession=profession.length
+const Vonage = require('@vonage/server-sdk')
 
-
-
-
-
-
-
-
-
-
-
-const twilio=require('twilio');
-let now = new Date(); 
-//console.log(now)
-const schedule = require('node-schedule');
-
-	const sdate=new Date(now)
-	sdate.setHours(17,0,0)//time to send message
-	//console.log(sdate)
-	schedule.scheduleJob(sdate, ()=>{
-		console.log('start');
-		mesP()
-	console.log('end');
-	});
-
- 
-var accountSid = process.env.TWILIO_ACCOUNT_SID; // Your Account SID from www.twilio.com/console
-var authToken = process.env.TWILIO_AUTH_TOKEN;   // Your Auth Token from www.twilio.com/console
-
-let Mymessage='judy modi'
-let Yournumber='+972523190543'
-let MyNumber='+12184141637'
-
-
-
-function mesP(){
-	console.log(".........")
-	var client=new twilio(accountSid,authToken)
-
-	client.messages
-	.create({
-		from:MyNumber,
-		to:Yournumber,
-		body:Mymessage
+const vonage = new Vonage({
+  apiKey: "7ac87cfe",
+  apiSecret: "Y6TADHof3zj4ODXh"
 })
-	.then(message=>console.log(message.sid)).done();
-	console.log(".........")
+
+
+const from = "School"
+const to = phone
+let text="Your kid BREEZEE from the class";
+vonage.message.sendSms(from, to, text, (err, responseData) => {
+    if (err) {
+        console.log(err);
+    } else {
+        if(responseData.messages[0]['status'] === "0") {
+            console.log("Message sent successfully.");
+        } else {
+            console.log(`Message failed with error: ${responseData.messages[0]['error-text']}`);
+        }
+    }
+})
+}
 }
 
 
+function SMS_SENDER_Breeze_from_School(){
+	console.log("in SMS_SENDER_Breeze_from_class()",sum_of_students_who_did_not_attend_class_and_not_in_school)
+	for(let i=0;i<sum_of_students_who_did_not_attend_class_and_not_in_school;i++)
+	{
+	let phone=List_of_students_who_did_not_attend_class_and_not_in_school[i].phone.replace(/0/, "+972")
+	
+	console.log(phone)
+	//let name=List_of_students_who_did_not_attend_class_but_is_in_school[i].name
+	//let profession=List_of_students_who_did_not_attend_class_but_is_in_school[i].profession
+	//let size_of_profession=profession.length
+	const Vonage = require('@vonage/server-sdk')
+	
+	const vonage = new Vonage({
+	  apiKey: "7ac87cfe",
+	  apiSecret: "Y6TADHof3zj4ODXh"
+	})
+	
+	
+	const from = "School"
+	const to = phone
+	let text="Your kid BREEZEE from the school";
+	vonage.message.sendSms(from, to, text, (err, responseData) => {
+		if (err) {
+			console.log(err);
+		} else {
+			if(responseData.messages[0]['status'] === "0") {
+				console.log("Message sent successfully.");
+			} else {
+				console.log(`Message failed with error: ${responseData.messages[0]['error-text']}`);
+			}
+		}
+	})
+	}
+	}
+	
 
 
 
 
 
+// const twilio=require('twilio');
+// var accountSid = process.env.TWILIO_ACCOUNT_SID; // Your Account SID from www.twilio.com/console
+// var authToken = process.env.TWILIO_AUTH_TOKEN;   // Your Auth Token from www.twilio.com/console
+
+// let Mymessage='judy modi'
+// let Yournumber='+972523190543'
+// let MyNumber='+12184141637'
 
 
+// mesP()
+// function mesP(){
+// 	console.log(".........")
+// 	var client=new twilio(accountSid,authToken)
 
-
-
-
-
-
-
-
-// function SMS_SENDER_Breeze_from_class(){
-// console.log("in SMS_SENDER_Breeze_from_class()",sum_of_students_who_did_not_attend_class_but_is_in_school)
-// for(let i=0;i<sum_of_students_who_did_not_attend_class_but_is_in_school;i++)
-// {
-// let phone=List_of_students_who_did_not_attend_class_but_is_in_school[i].phone.replace(/0/, "+972")
-// //let name=List_of_students_who_did_not_attend_class_but_is_in_school[i].name
-// //let profession=List_of_students_who_did_not_attend_class_but_is_in_school[i].profession
-// //let size_of_profession=profession.length
-// const Vonage = require('@vonage/server-sdk')
-
-// const vonage = new Vonage({
-//   apiKey: "7ac87cfe",
-//   apiSecret: "Y6TADHof3zj4ODXh"
+// 	client.messages
+// 	.create({
+// 		from:MyNumber,
+// 		to:Yournumber,
+// 		body:Mymessage
 // })
-
-
-// const from = "School"
-// const to = phone
-// let text="Your kid BREEZEE from the class";
-// vonage.message.sendSms(from, to, text, (err, responseData) => {
-//     if (err) {
-//         console.log(err);
-//     } else {
-//         if(responseData.messages[0]['status'] === "0") {
-//             console.log("Message sent successfully.");
-//         } else {
-//             console.log(`Message failed with error: ${responseData.messages[0]['error-text']}`);
-//         }
-//     }
-// })
-// }
+// 	.then(message=>console.log(message.sid)).done();
+// 	console.log(".........")
 // }
 
 
 
-// function SMS_SENDER_Breeze_from_School(){
-// 	console.log("in SMS_SENDER_Breeze_from_class()",sum_of_students_who_did_not_attend_class_and_not_in_school)
-// 	for(let i=0;i<sum_of_students_who_did_not_attend_class_and_not_in_school;i++)
-// 	{
-// 	let phone=List_of_students_who_did_not_attend_class_and_not_in_school[i].phone.replace(/0/, "+972")
-// 	//let name=List_of_students_who_did_not_attend_class_but_is_in_school[i].name
-// 	//let profession=List_of_students_who_did_not_attend_class_but_is_in_school[i].profession
-// 	//let size_of_profession=profession.length
-// 	const Vonage = require('@vonage/server-sdk')
-	
-// 	const vonage = new Vonage({
-// 	  apiKey: "7ac87cfe",
-// 	  apiSecret: "Y6TADHof3zj4ODXh"
-// 	})
-	
-	
-// 	const from = "School"
-// 	const to = phone
-// 	let text="Your kid BREEZEE from the school";
-// 	vonage.message.sendSms(from, to, text, (err, responseData) => {
-// 		if (err) {
-// 			console.log(err);
-// 		} else {
-// 			if(responseData.messages[0]['status'] === "0") {
-// 				console.log("Message sent successfully.");
-// 			} else {
-// 				console.log(`Message failed with error: ${responseData.messages[0]['error-text']}`);
-// 			}
-// 		}
-// 	})
-// 	}
-// 	}
-	
+
+
+// //  //הוספה של משתנה חדש ללסון
+// // new LessonSchema({
+// // 	className:"א2",
+// // 	profession:"חשבון",
+// // 	day:today,
+// // 	time:Ttime,
+// // 	students:[
+// // 		{
+// // 		name:"סיטארה אלייב",
+// // 		arrived:true
+// // 		},
+// // 		{
+// // 		name:"דודי ביטון",
+// // 		arrived:true
+// // 		},
+// // 		{
+// // 		name:"יהודית מנדלבוים",
+// // 		arrived:false
+// // 		}
+// // 	]
+
+// // }).save()
+
+// let h="י-י"
+// let d="י י"
+// let a="ר"
+
+// a=h.replace(/\-/, ' ')
+// console.log(a);
+
+// //console.log (d.localeCompare(h,'he'))
+
+
+
+
