9a35c78cbde80ac79cc915653f37891f0f31b384
יהודית מנדלבוים <judymand70@gmail.com>
2022-05-29 21:15:26 +0300

    Changing the design of a card component.

--- components/Card.js
+++ components/Card.js
@@ -9,15 +9,15 @@ export const Card = (props) => {
 
   return (
     <View style={style.viewContainerCard}>
-       {/* <ScrollView style={style.card}> */}
-      <View style={[style.card, device ? style.cardIOS : style.cardAndroid]}>
       
+      <View style={[style.card, device ? style.cardIOS : style.cardAndroid]}>
+      <ScrollView style={{flexGrow: 0}}>
          <KeyboardAvoidingView  behavior={Behavior} keyboardVerticalOffset={30}> 
             {props.children}
          </KeyboardAvoidingView>
-        
+         </ScrollView>
        </View>
-       {/* </ScrollView> */}
+       
     </View>
        
   );
