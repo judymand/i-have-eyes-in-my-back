54e0892e1656d715a64fb6d7f3e9f871036ef6ea
יהודית מנדלבוים <judymand70@gmail.com>
2022-06-02 23:03:25 +0300

    fixed alert show two buttons.

--- components/ShowAlert.js
+++ components/ShowAlert.js
@@ -2,6 +2,15 @@ import { Alert } from 'react-native';
 
 export const ShowAlert = (props, resData, resData2) => {
 
+  let text2 = null
+
+  if(!!resData2){
+      text2 = { 
+        text:  !resData2.textButton ? {} : resData2.textButton, 
+        onPress: () => !resData2.pageName ? {} : props.navigation.navigate(resData2.pageName)  
+      }
+    }
+
     Alert.alert(
       resData.message,
       resData.text,
@@ -10,10 +19,7 @@ export const ShowAlert = (props, resData, resData2) => {
         text: !resData.textButton ? "הבנתי" : resData.textButton, 
         onPress: () => resData.pageName === "" || !resData.pageName ? {} : props.navigation.navigate(resData.pageName),     
       },
-      { 
-        text:  !resData2 ? "" : resData2.message, 
-        onPress: () => !resData2 ? {} : props.navigation.navigate(resData2.pageName),     
-      },
+      text2
     ]
     )
 
